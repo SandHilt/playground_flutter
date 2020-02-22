fcc753bcb24d0f0461713087c3fc63ed763fde74
Bruno Ochotorena <hilt@outlook.com.br>
2020-02-22 01:34:33 -0300

    Adicionando scroll infinito

--- lib/main.dart
+++ lib/main.dart
@@ -7,23 +7,49 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Seja bem vindo ao Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Bem vindo ao Flutter, parceiro!'),
-        ),
-        body: Center(child: PalavrasAleatorias()),
-      ),
+      title: 'Gerador de nomes',
+      home: PalavrasAleatorias(),
     );
   }
 }
 
 class PalavrasAleatoriasState extends State<PalavrasAleatorias> {
- @override
+  final _sugestoes = <WordPair>[];
+  final _fonteGrande = const TextStyle(fontSize: 18.0);
+
+  Widget _buildLinha(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _fonteGrande,
+      ),
+    );
+  }
+
+  Widget _buildSugestoes() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return Divider();
+
+        final index = i ~/ 2;
+        if (index >= _sugestoes.length) {
+          _sugestoes.addAll(generateWordPairs().take(10));
+        }
+        return _buildLinha(_sugestoes[index]);
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
-  }  
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gerador de nomes maneiros'),
+      ),
+      body: _buildSugestoes(),
+    );
+  }
 }
 
 class PalavrasAleatorias extends StatefulWidget {
