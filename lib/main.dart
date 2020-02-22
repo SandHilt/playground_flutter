79bf3808f38ab3fc62dfdd1f647ac87bd8e2b7c8
Bruno Ochotorena <hilt@outlook.com.br>
2020-02-21 22:13:38 -0300

    Adicionando palavras aleatórias

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Seja bem vindo ao Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: Text('Bem vindo ao Flutter, parceiro!'),
         ),
-        body: Center(child: Text('E aí, mundo!')),
+        body: Center(child: Text(wordPair.asPascalCase)),
       ),
     );
   }
